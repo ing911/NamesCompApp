54ec2c6ddd82870fbbdce6130a8051edb869d310
ing911 <axmed.xamchiev@gmail.com>
2023-05-19 01:11:20 +0300

    Реализована логика двух экранов
    Добавил по видео-уроку два метода в ResultVC, а также во viewDidLoad добавлены все необходимые настройки для корректного отображения информации на экране; во FirstVC добавлено расширение для класса, где вызваны два метода, чтобы можно было скрывать тапом клав-ру и переходить с одного firstName textfield на второй, а затем далее на второй экран; переход по кнопке на второй экран осуществляется, но не срабатывает переход с первого textField на второй и со второго textField на след экран. Также, в main storyboard обоих экранов, согласно уроку, проставил пункты в Attributes inspector в полях return key: "next" и "done" соответственно и протянул лучи от обоих textField к First View Controller и выбрал пункт "delegate"

--- NamesCompApp/FirstViewController.swift
+++ NamesCompApp/FirstViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    
     @IBOutlet weak var yourNameTF: UITextField!
     @IBOutlet weak var partnerNameTF: UITextField!
     
@@ -16,15 +16,32 @@ class FirstViewController: UIViewController {
         super.viewDidLoad()
         
     }
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let destinationVC = segue.destination as? ResultViewController else { return }
         destinationVC.firstName = yourNameTF.text
         destinationVC.secondName = partnerNameTF.text
     }
-
+    
     @IBAction func resultButtonTapped() {
         performSegue(withIdentifier: "goToResult", sender: nil)
     }
 }
 
+extension FirstViewController: UITextViewDelegate {
+    // метод, позвол. скрывать клав-ру тапом по экрану:
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event) // вызываем метод у супер-класса
+        view.endEditing(true)
+    }
+    // метод, позвол. при помощи экранной клав-ры переходить с 1-го поля на 2-е поле, а затем
+    // по нажатию на кнопку "да" вызывать метод для перехода на 2-й экран
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == yourNameTF {
+            partnerNameTF.becomeFirstResponder()
+        } else {
+            resultButtonTapped()
+        }
+        return true
+    }
+}
--- NamesCompApp/ResultViewController.swift
+++ NamesCompApp/ResultViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ResultViewController: UIViewController {
-
+    
     @IBOutlet weak var namesLabel: UILabel!
     @IBOutlet weak var progressView: UIProgressView!
     @IBOutlet weak var resultLabel: UILabel!
@@ -16,15 +16,53 @@ class ResultViewController: UIViewController {
     var firstName: String?
     var secondName: String?
     
+    private var resultValue = 0 // используется для расчёта результата совместимости имён
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-    namesLabel.text = "\(firstName ?? "") and \(secondName ?? "")"
-
-       
+        
+        resultValue = findResult() // получаем нужное число ("соответстивие" имён)
+        namesLabel.text = "\(firstName ?? "") and \(secondName ?? "")" // тест с именами
+        resultLabel.text = resultValue.formatted(.percent) // число в процентах %
+        progressView.progress = Float(resultValue) / 100 // шкала прогресса
+        progressView.transform = progressView.transform.scaledBy(x: 1, y: 4) // делаем progressView толще и заметнее
     }
     
-
+    // функция, с помощью которой мы получаем некоторое число для имени
+    private func findValue(for name: String) -> Int {
+        var count = 0
+        
+        for character in name.lowercased() {
+            switch character {
+            case "a", "i", "j", "q", "y": count += 1
+            case "b", "k", "r": count += 2
+            case "c", "g", "l", "s": count += 3
+            case "d", "m", "t": count += 4
+            case "e", "h", "n", "x": count += 5
+            case "u", "v", "w": count += 6
+            case "o", "z": count += 7
+            case "f", "p": count += 8
+            default: count += 0
+            }
+        }
+        return count
+    }
     
-
+    // функция, с помощью которой получаем разницу между именами в числовом формате ("соответствие")
+    private func findResult() -> Int {
+        var result = 0
+        
+        let firstValue = findValue(for: firstName ?? "Error")
+        let secondValue = findValue(for: secondName ?? "Error")
+        let absDifference = abs(firstValue - secondValue)
+        
+        switch absDifference {
+        case 0, 1, 2: result = 100
+        case 3, 4: result = 65
+        case 5: result = 50
+        case 6: result = 40
+        default: result = 30
+        }
+        return result
+    }
 }
